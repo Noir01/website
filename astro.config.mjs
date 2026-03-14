15dc6bafda20c1e2cd2d7c55af43ba0cd92ac534
Aniruddh S. <noirrr@duck.com>
2026-03-13 20:12:14 -0700

    Ditch Vercel. Move to Cloudflare
    Friendship ended with Vercel
    Now
    Cloudflare
    is my
    best friend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  adapter: cloudflare(),
 });
