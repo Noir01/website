2eec508a868ef4020f41684e14de27e85d716b46
Aniruddh S. <noirrr@duck.com>
2026-03-14 10:20:48 -0700

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://srivastava.dev',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
