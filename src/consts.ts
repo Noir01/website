e450a76a7dd30802e2f2b84a6f92bdb8be782957
Aniruddh S. <noirrr@duck.com>
2026-03-27 23:50:53 -0700

    Fix title

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = 'srivastava.dev';
+export const TITLE = 'Aniruddh Srivastava | Purdue | Cloudflare';
 export const SPOTIFY_API = 'https://spotify.noir.ac';
 export const LASTFM_API = 'https://lastfm.noir.ac';
