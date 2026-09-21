81e0d5203042d0848ce7ebb1d6d5215e038c0fbf
Aniruddh S <noirrr@duck.com>
2026-09-20 21:22:10 -0400

    content: refresh homepage profile

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'srivastava.dev';
-export const TITLE = 'Aniruddh Srivastava | Purdue | Cloudflare';
+export const TITLE = 'Aniruddh Srivastava | Purdue';
 export const SPOTIFY_API = 'https://spotify.noir.ac';
 export const LASTFM_API = 'https://lastfm.noir.ac';
